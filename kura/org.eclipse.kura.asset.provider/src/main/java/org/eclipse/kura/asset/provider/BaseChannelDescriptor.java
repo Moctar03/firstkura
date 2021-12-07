53886acea6aa5d5c0e7f73e207097e74df69fee8
Nicola Timeus <nicola.timeus@eurotech.com>
2021-12-07 18:20:13 +0100

    Updated copyright headers
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.asset.provider/src/main/java/org/eclipse/kura/asset/provider/BaseChannelDescriptor.java
+++ kura/org.eclipse.kura.asset.provider/src/main/java/org/eclipse/kura/asset/provider/BaseChannelDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2016, 2021 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -130,7 +130,7 @@ public class BaseChannelDescriptor implements ChannelDescriptor {
         addOptions(valueType, DataType.values());
 
         this.defaultElements.add(valueType);
-        
+
         final Tad valueUnit = new Tad();
         valueUnit.setName(VALUE_UNIT.value().substring(1));
         valueUnit.setId(VALUE_UNIT.value());
