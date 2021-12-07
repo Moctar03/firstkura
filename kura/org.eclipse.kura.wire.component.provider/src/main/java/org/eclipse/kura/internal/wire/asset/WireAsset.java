062638659b31ae54a5199065cb4c322ae367d05b
Nicola Timeus <nicola.timeus@eurotech.com>
2021-12-07 18:10:31 +0100

    Added support for emitting unit on channel event
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAsset.java
+++ kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAsset.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2016, 2021 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -401,7 +401,18 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
             if (WireAsset.this.options.emitAllChannels()) {
                 emitAllReadChannels();
             } else {
-                emitChannelRecords(Collections.singletonList(event.getChannelRecord()));
+                final ChannelRecord eventRecord = event.getChannelRecord();
+
+                if (eventRecord.getUnit() == null || eventRecord.getUnit().isEmpty()) {
+                    final Channel channel = getAssetConfiguration().getAssetChannels()
+                            .get(eventRecord.getChannelName());
+
+                    if (channel != null) {
+                        eventRecord.setUnit(channel.getUnit());
+                    }
+                }
+
+                emitChannelRecords(Collections.singletonList(eventRecord));
             }
         }
 
